36ff3c6fedb1f8b033a77247d1b612a8bf5c5012
UllrS <94692310+UllrS@users.noreply.github.com>
2022-09-15 00:02:06 +0300

    Update LinkViewUllr.swift

--- LinkViewUllr.swift
+++ LinkViewUllr.swift
@@ -2,26 +2,64 @@
 import SwiftUI
 import LinkPresentation
 
-struct LinkViewUllr: View{
+
+struct PreloadView: View{
 
     let url: URL
     
     //LinkView parameters
-    let linkVideo: Bool?
-    let linkImage: Bool?
-    let linkIcon: Bool?
     var body: some View{
         LinkPreview(url: url)
+            .aspectRatio(1.0, contentMode: .fit)
     }
 }
 
 class LinkPreviewSizeController: LPLinkView {
-    override var intrinsicContentSize: NSSize { NSSize(width: 150, height: super.intrinsicContentSize.height) }   //super.intrinsicContentSize.height
+//    override var intrinsicContentSize: Size { UISize(width: 150, height: super.intrinsicContentSize.height) }
 }
 
+#if os(iOS)
+struct LinkPreview: UIViewRepresentable {
+    typealias UIViewType = LinkPreviewSizeController
+    
 
+    let url: URL
+    let linkVideo: Bool = true
+    let linkImage: Bool = true
+    let linkIcon: Bool = true
+    
+    func makeUIView(context: Context) -> LinkPreviewSizeController {
+        return LinkPreviewSizeController(url: url)
+    }
+
+    func updateUIView(_ nsView: LinkPreviewSizeController, context: Context) {
+        let provider = LPMetadataProvider()
+
+        provider.startFetchingMetadata(for: url) { metadata, error in
+            guard let metaData = metadata, error == nil else {
+                return
+            }
+            DispatchQueue.main.async {
+                if !self.linkVideo{
+                    metaData.videoProvider = nil
+                }
+                if !self.linkIcon{
+                    metaData.iconProvider = nil
+                }
+                if !self.linkImage{
+                    metaData.imageProvider = nil
+                }
+                nsView.metadata = metaData
+            }
+        }
+    }
+}
+
+#elseif os(OSX)
+import AppKit
 struct LinkPreview: NSViewRepresentable {
     typealias NSViewType = LinkPreviewSizeController
+    
 
     let url: URL
     let linkVideo: Bool = true
@@ -44,13 +82,14 @@ struct LinkPreview: NSViewRepresentable {
                     metaData.videoProvider = nil
                 }
                 if !self.linkIcon{
-                    metaData.videoProvider = nil
+                    metaData.iconProvider = nil
                 }
                 if !self.linkImage{
-                    metaData.videoProvider = nil
+                    metaData.imageProvider = nil
                 }
                 nsView.metadata = metaData
             }
         }
     }
 }
+#endif
